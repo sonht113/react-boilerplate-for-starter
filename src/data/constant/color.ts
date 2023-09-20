0e66690a900cb8440e10ff4b950f15c5a3973c2d
trongson <trongson18101999@gmail.com>
2023-09-20 22:22:42 +0700

    feats: create color constant

---
+++ src/data/constant/color.ts
@@ -0,0 +1,11 @@
+const COLOR = {
+  ACTIVE: '#34A853',
+  LOCKED: '#F31111',
+  PAUSE: '#FA8C16',
+  DISABLED: '#96969B',
+  LIGHT_PRIMARY: '#C67F03',
+  DARK_PRIMARY: '#3b78f9',
+  LOGIN_BG: '#162C5B',
+};
+
+export default COLOR;
