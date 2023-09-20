2a8acf99a07d24cbc5f34e598953aeefe6634420
trongson <trongson18101999@gmail.com>
2023-09-20 22:21:38 +0700

    feats:(mode-theme) features add mode theme for project

---
+++ src/hooks/theme-store/use-theme-store.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+
+type ThemeStore = {
+  theme: 'dark' | 'light';
+  setTheme: (_: { theme: ThemeStore['theme'] }) => void;
+};
+
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+  ? 'dark'
+  : 'light';
+const userTheme = localStorage.getItem('theme') as ThemeStore['theme'];
+
+const useThemeStore = create<ThemeStore>()((set) => ({
+  theme: userTheme || 'light' || systemTheme,
+  setTheme: ({ theme }: { theme: ThemeStore['theme'] }) => {
+    const body = document.body;
+
+    if (theme === 'dark') {
+      if (!body.hasAttribute('class')) {
+        body.setAttribute('class', 'dark');
+      }
+    } else {
+      if (body.hasAttribute('class')) {
+        body.removeAttribute('class');
+      }
+    }
+
+    set({
+      theme,
+    });
+  },
+}));
+
+export default useThemeStore;
