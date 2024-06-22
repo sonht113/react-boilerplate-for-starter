5ad4ef5fa4252041d2d4d4eff4b3082a51ca64d6
trongson <trongson18101999@gmail.com>
2024-06-22 13:33:50 +0700

    fix: fix layout

---
+++ src/layout/footer/index.tsx
@@ -0,0 +1,14 @@
+const FooterComponent = () => {
+  return (
+    <footer className="w-full h-[200px] dark:bg-neutral-700 bg-slate-300">
+      <div className="w-full h-full flex justify-center items-center">
+      <div className="flex justify-center items-center gap-4">
+        <img src="/vite.svg" alt="logo" />
+        <span>Footer-ReactJS Boilerplate By TrongSon</span>
+      </div>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterComponent;
